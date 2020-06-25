635335ee0b29865b7d1dd0ada84f31fac9e3ba76
A601-22 <tnk.dat.class@gmail.com>
2020-06-25 11:15:11 +0900

    エラーの確認

--- v0625/Form1.cs
+++ v0625/Form1.cs
@@ -16,5 +16,11 @@ namespace v0625
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 5;
+            MessageBox.Show("" + (10 / a));
+        }
     }
 }
